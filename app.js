34d67201f8312fce2431e494bb98713cd2dff29f
Nathaniel Cuenca <ncuenca.nac@gmail.com>
2021-07-28 12:47:42 -0700

    finished

--- app.js
+++ app.js
@@ -1,21 +1,60 @@
+"use strict"
+
 /** Simple demo Express app. */
 
 const express = require("express");
 const app = express();
+const { findMean, findMedian, findMode } = require("./stats")
+const { convertStrNums } = require('./utils')
 
 // useful error class to throw
-const { NotFoundError } = require("./expressError");
+const { NotFoundError, BadRequestError } = require("./expressError");
 
 const MISSING = "Expected key `nums` with comma-separated list of numbers.";
 
 
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
-
+app.get("/mean", function (req, res, next) {
+  if (Object.keys(req.query).length === 0) {
+    throw new BadRequestError("Numbers are required");
+  }
+  let strNums = req.query.nums.split(",");
+  let nums = convertStrNums(strNums);
+  if (nums.includes(NaN)) {
+    let invalid = strNums[nums.findIndex(Number.isNaN)];
+    throw new BadRequestError(`${invalid} is not a number`);
+  }
+  return res.json({operation: "mean", value: findMean(nums)});
+});
 
 /** Finds median of nums in qs: returns {operation: "median", result } */
+app.get("/median", function (req, res, next) { 
+  if (Object.keys(req.query).length === 0) {
+    throw new BadRequestError("Numbers are required");
+  }
+  let strNums = req.query.nums.split(",");
+  let nums = convertStrNums(strNums);
+  if (nums.includes(NaN)) {
+    let invalid = strNums[nums.findIndex(Number.isNaN)];
+    throw new BadRequestError(`${invalid} is not a number`);
+  }
+  return res.json({operation: "median", value: findMedian(nums)});
+});
 
 
 /** Finds mode of nums in qs: returns {operation: "mean", result } */
+app.get("/mode", function (req, res, next) {
+  if (Object.keys(req.query).length === 0) {
+    throw new BadRequestError("Numbers are required");
+  }
+  let strNums = req.query.nums.split(",");
+  let nums = convertStrNums(strNums);
+  if (nums.includes(NaN)) {
+    let invalid = strNums[nums.findIndex(Number.isNaN)];
+    throw new BadRequestError(`${invalid} is not a number`);
+  }
+  return res.json({operation: "mode", value: findMode(nums)});
+});
 
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
--- utils.js
+++ utils.js
@@ -4,8 +4,11 @@ const { BadRequestError } = require("./expressError");
 /** Convert strNums like ["1","2","3"] to [1, 2, 3]. */
 
 function convertStrNums(strNums) {
-  // if the conversion isn't successful, throw a BadRequestError and will
-  // be handled in your route
+  nums = []
+  for (idx in strNums) {
+    nums.push(Number(strNums[idx]));
+  }
+  return nums;
 }
 
 
